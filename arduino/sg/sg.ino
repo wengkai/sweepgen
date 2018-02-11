d07c9175d1bf07d074ed5e289ce797b62dee7054
Weng Kai <wengkai@zju.edu.cn>
2018-02-11 23:06:01 +0800

    Switch from Adafruit 5351 lib to Etherkit 5351 lib

--- arduino/sg/sg.ino
+++ arduino/sg/sg.ino
@@ -1,6 +1,8 @@
-#include <Adafruit_SI5351.h>
-#include <asserts.h>
-#include <errors.h>
+#include <si5351.h>
+
+// #include <Adafruit_SI5351.h>
+// #include <asserts.h>
+// #include <errors.h>
 
 #include <U8g2lib.h>
 #include <U8x8lib.h>
@@ -21,7 +23,7 @@ RotateEncoder re(2,3);
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
 
 //  5351------------------------------
-Adafruit_SI5351 clockgen = Adafruit_SI5351();
+// Adafruit_SI5351 clockgen = Adafruit_SI5351();
 
 void showPrompt(const char *s)
 {
@@ -29,12 +31,7 @@ void showPrompt(const char *s)
   do {
     u8g2.setFont(u8g2_font_ncenB18_tr);
     u8g2.drawStr(32,50,s);
-    // Serial.println("p");
   } while ( u8g2.nextPage() );
-  // u8g2.clearBuffer();					// clear the internal memory
-  // u8g2.setFont(u8g2_font_ncenB14_tr);	// choose a suitable font
-  // u8g2.drawStr(32,50,"hello");	// write something to the internal memory
-  // u8g2.sendBuffer();					// transfer internal memory to the display
 }
 
 void showFreq(const char* mode, unsigned long freq, int position)
@@ -55,17 +52,55 @@ void showFreq(const char* mode, unsigned long freq, int position)
   } while ( u8g2.nextPage() );
 }
 
+Si5351 si5351;
+
 void setup() {
   Serial.begin(9600);
   u8g2.begin(); 
-  if (clockgen.begin() != ERROR_NONE)
-  {
-    /* There was a problem detecting the IC ... check your connections */
-    Serial.print("Ooops, no Si5351 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-  Serial.println("OK!");
+  // if (clockgen.begin() != ERROR_NONE)
+  // {
+  //   /* There was a problem detecting the IC ... check your connections */
+  //   Serial.print("Ooops, no Si5351 detected ... Check your wiring or I2C ADDR!");
+  //   while(1);
+  // }
+  // Serial.println("OK!");
+  // /* INTEGER ONLY MODE --> most accurate output */  
+  // /* Setup PLLA to integer only mode @ 900MHz (must be 600..900MHz) */
+  // /* Set Multisynth 0 to 112.5MHz using integer only mode (div by 4/6/8) */
+  // /* 25MHz * 36 = 900 MHz, then 900 MHz / 8 = 112.5 MHz */
+  // Serial.println("Set PLLA to 900MHz");
+  // clockgen.setupPLLInt(SI5351_PLL_A, 36);
+  // Serial.println("Set Output #0 to 112.5MHz");  
+  // clockgen.setupMultisynthInt(0, SI5351_PLL_A, SI5351_MULTISYNTH_DIV_8);
+
+  // /* FRACTIONAL MODE --> More flexible but introduce clock jitter */
+  // /* Setup PLLB to fractional mode @616.66667MHz (XTAL * 24 + 2/3) */
+  // /* Setup Multisynth 1 to 13.55311MHz (PLLB/45.5) */
+  // clockgen.setupPLL(SI5351_PLL_B, 24, 2, 3);
+  // Serial.println("Set Output #1 to 13.553115MHz");  
+  // clockgen.setupMultisynth(1, SI5351_PLL_B, 45, 1, 2);
+
+  // /* Multisynth 2 is not yet used and won't be enabled, but can be */
+  // /* Use PLLB @ 616.66667MHz, then divide by 900 -> 685.185 KHz */
+  // /* then divide by 64 for 10.706 KHz */
+  // /* configured using either PLL in either integer or fractional mode */
+
+  // Serial.println("Set Output #2 to 10.706 KHz");  
+  // clockgen.setupMultisynth(2, SI5351_PLL_B, 900, 0, 1);
+  // clockgen.setupRdiv(2, SI5351_R_DIV_64);
+    
+  // /* Enable the clocks */
+  // clockgen.enableOutputs(true);
   showPrompt("SG-1");
+  boolean i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
+  if(!i2c_found)
+  {
+    Serial.println("Device not found on I2C bus!");
+  }
+
+  // Set CLK0 to output 14 MHz
+  si5351.set_freq(1400000000ULL, SI5351_CLK0);
+  si5351.update_status();
 }
 
 int mode = 0;
